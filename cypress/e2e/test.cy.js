29827219e3bde482a37ba0e5bd4dc780e9e945b7
Hasan <hasanazeerkhan@gmail.com>
2024-10-11 17:40:00 +0530

    feat: initialized the repo with test cases

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,65 @@
+describe("Demo QA - Elements", function () {
+  beforeEach(function () {
+    cy.visit("/");
+  });
+  it("Text box", function () {});
+  it("Check box", function () {});
+  it("Radio button", function () {});
+  it("Web tables", function () {});
+  it("Buttons", function () {});
+  it("Links", function () {});
+  it("Broken links - Image", function () {});
+  it("Upload and Download", function () {});
+});
+
+describe("Demo QA - Forms", function () {
+  beforeEach(function () {
+    cy.visit("/");
+  });
+  it("Practice Form", function () {});
+});
+
+describe("Demo QA - Alert, Frame and Windows", function () {
+  beforeEach(function () {
+    cy.visit("/");
+  });
+  it("Browser Windows", function () {});
+  it("Alerts", function () {});
+  it("Frames", function () {});
+  it("Nested frames", function () {});
+  it("Modal Dialogs", function () {});
+});
+
+describe("Demo QA - WIdgets", function () {
+  beforeEach(function () {
+    cy.visit("/");
+  });
+  it("Accordian", function () {});
+  it("Auto Complete", function () {});
+  it("Date Picker", function () {});
+  it("Progress Bar", function () {});
+  it("Tool Tips", function () {});
+  it("Menu", function () {});
+  it("Select Menu", function () {});
+});
+
+describe("Demo QA - Interactions", function () {
+  beforeEach(function () {
+    cy.visit("/");
+  });
+  it("Sortable", function () {});
+  it("Selectable", function () {});
+  it("Resizable", function () {});
+  it("Droppable", function () {});
+  it("Dragabble", function () {});
+});
+
+describe("Demo QA - Book Store Application", function () {
+  beforeEach(function () {
+    cy.visit("/");
+  });
+  it("Login", function () {});
+  it("Book Store", function () {});
+  it("Profile", function () {});
+  it("Book store API", function () {});
+});
